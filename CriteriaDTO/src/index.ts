4fdc5dc58ed6183d3025b9d2cca22e21e6d54081
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-04-27 11:55:23 -0500

    feat: add item support

---
+++ CriteriaDTO/src/index.ts
@@ -0,0 +1,4 @@
+import jsonData from './data/Items1.json'
+
+console.log(jsonData)
+console.log('hello world');
---
+++ CriteriaDTO/src/modules/items/domain/Item.ts
@@ -0,0 +1,8 @@
+export interface Item {
+    id: string;
+    name: string;
+    prime: boolean;
+    new: boolean;
+    price: number;
+    currency: string;
+}
